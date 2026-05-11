b6e9386812076bb644910e521a6b017ee4913648
joelkirapash-lang <joelkirapash@gmail.com>
2026-05-12 01:41:16 +0300

    Rename all component files to jsx and simplify vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -3,22 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: { '.js': 'jsx' }
-    }
-  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.js'],
     include: ['src/**/*.{test,spec}.{js,jsx}'],
-    transformMode: {
-      web: [/\.[jt]sx?$/]
-    },
   },
 })
