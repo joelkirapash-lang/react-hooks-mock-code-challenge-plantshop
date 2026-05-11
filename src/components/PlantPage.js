bee45c62de82191f788f9c1e3370d4cb6e2528ff
joelkirapash-lang <joelkirapash@gmail.com>
2026-05-12 00:21:25 +0300

    Implement plant fetch, form submission, sold out toggle, and search filter

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants((prev) => [...prev, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchQuery={searchQuery} onSearchChange={setSearchQuery} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
