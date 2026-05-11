698eb6bce385e9bcb82cdb0b9ad573a04051df23
joelkirapash-lang <joelkirapash@gmail.com>
2026-05-12 01:20:30 +0300

    Fix JSX in .js files and vitest config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: ['./src/setupTests.js'],
+    include: ['src/**/*.{test,spec}.{js,jsx}'],
+    transformMode: { web: [/\.[jt]sx?$/] },
   },
 })
--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
